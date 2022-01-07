d45eee7549c7d21c2ec7bc3e160584bb0c5ed1d4
dkwilson <general@ytechnology.net>
2022-01-07 18:45:37 -0500

    added activity view and context menu

---
+++ FoodPin/View/ActivityView.swift
@@ -0,0 +1,23 @@
+//
+//  ActivityView.swift
+//  FoodPin (iOS)
+//
+//  Created by Dkeith Wilson on 1/7/22.
+//
+
+import SwiftUI
+
+struct ActivityView: UIViewControllerRepresentable {
+    
+    var activityItems: [Any]
+    var applicationActivities: [UIActivity]? = nil
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let activityController = UIActivityViewController(activityItems: activityItems, applicationActivities: applicationActivities)
+        return activityController
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+}
