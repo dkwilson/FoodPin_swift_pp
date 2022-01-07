b8d6a81a1b1df1bc9dbace48145278346540e57a
dkwilson <general@ytechnology.net>
2022-01-07 15:14:45 -0500

    Initial Commit

---
+++ Shared/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  Shared
+//
+//  Created by Dkeith Wilson on 1/7/22.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
